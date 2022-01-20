4729e6a25310e7e0535b5a4b4e56f862919b8442
a.chebykin <a.chebykin@betinvest.com>
2022-01-20 13:52:54 +0200

    add Lviv in console

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ const kyivWeather = new Weather('Kiev', {temp_c: 12, condition: 'clouds'});
 const Lviv = new Weather('Lviv', {temp_c: 7, condition: 'rain'})
 document.body.innerHTML = kyivWeather.temp_c; // выведет в консоль температуру
 console.log(`Температура в ${kyivWeather.city} - ${kyivWeather.temp_c}`);
+console.log(`Температура в ${Lviv.city} - ${Lviv.temp_c}`);
 kyivWeather.removeClouds();
 console.log(kyivWeather.condition) // выведет в консоль sun
