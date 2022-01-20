ef3a7853606a7d17f581e5ef8cf283bd76a669c2
a.chebykin <a.chebykin@betinvest.com>
2022-01-20 13:38:31 +0200

    add params to Lviv

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function Weather(city, weatherConditions = 'Данных о погоде нет') {
+function Weather(city, weatherConditions) {
     this.city = city
     this.temp_c = weatherConditions.temp_c
     this.condition = weatherConditions.condition
-    if (typeof weatherConditions === "string"){
-        console.log(weatherConditions)
-    }
 }
 Weather.prototype.removeClouds = function () {
     this.condition = 'sun'
 }
 
 const kyivWeather = new Weather('Kiev', {temp_c: 12, condition: 'clouds'});
-const Lviv = new Weather('Lviv')
+const Lviv = new Weather('Lviv', {temp_c: 7, condition: 'rain'})
 document.body.innerHTML = kyivWeather.temp_c; // выведет в консоль температуру
 console.log(`Температура в ${kyivWeather.city} - ${kyivWeather.temp_c}`);
 kyivWeather.removeClouds();
